acab9d97c1037f29d013d4b7e9a731c6496252d3
Ranjeet Yadav <25030861@quantumuniversity.edu.in>
2026-03-23 00:05:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Simple scroll animation
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        let position = card.getBoundingClientRect().top;
+        let screen = window.innerHeight;
+
+        if(position < screen - 100){
+            card.style.opacity = 1;
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
